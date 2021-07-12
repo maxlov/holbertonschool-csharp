db3f960f1a4d47932bfc9af0ee522013a1c0d961
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-12 17:21:05 -0400

    code_fix double formatting syntax

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-		Console.WriteLine("Percent: {0}", percent.ToString("P"));
-        Console.WriteLine("Currency: {0}", currency.ToString("C2"));
+		Console.WriteLine($"Percent: {percent:0.00%}");
+        Console.WriteLine($"Currency: {currency:$#.00}");
 	}
 }
