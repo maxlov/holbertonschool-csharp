792b74aeafb5579926b380acdb8ec0317ad6cdb4
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-12 14:03:31 -0400

    code_fix dotnet run syntax

--- 0x00-csharp-hello_world/2-run_new_project.sh
+++ 0x00-csharp-hello_world/2-run_new_project.sh
@@ -2,4 +2,4 @@
 # creates new project with console templates then builds and runs it
 dotnet new console -o 2-new_project
 dotnet build 2-new_project
-dotnet run 2-new_project
+dotnet run --project 2-new_project/2-new_project.csproj
