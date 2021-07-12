6f938587de47487cda10f969bcc11444af0b837d
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-12 17:25:34 -0400

    code_fix now changed print syntax again

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-		Console.WriteLine($"Percent: {percent:0.00%}");
-        Console.WriteLine($"Currency: {currency:$#.00}");
+		Console.WriteLine($"Percent: {percent:P}");
+		Console.WriteLine($"Currency: {currency:C}");
 	}
 }
