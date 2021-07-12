b41fdd0d78afc6de8a570b6f0a60fcacc478d351
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-12 13:58:48 -0400

    code_add tasks 1 and 2

--- 0x00-csharp-hello_world/1-build_new_project.sh
+++ 0x00-csharp-hello_world/1-build_new_project.sh
@@ -1,1 +1,4 @@
 #!/usr/bin/env bash
+# creates new project with console templates then builds it
+dotnet new console -o 1-new_project
+dotnet build 1-new_project
--- 0x00-csharp-hello_world/2-run_new_project.sh
+++ 0x00-csharp-hello_world/2-run_new_project.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+# creates new project with console templates then builds and runs it
+dotnet new console -o 2-new_project
+dotnet build 2-new_project
+dotnet run 2-new_project
