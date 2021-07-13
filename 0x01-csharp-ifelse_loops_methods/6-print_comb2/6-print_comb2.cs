5d798e9942aafd1101a4f035e291469602abb181
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-13 12:30:33 -0400

    code_add 6-print_comb2 prints all combinations of two digits

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var end = ", ";
+        for (var tens = 0; tens <= 8; tens++)
+            for (var ones = tens + 1; ones <= 9; ones++)
+            {
+                if (tens == 8 && ones == 9)
+                    end = "\n";
+                Console.Write($"{tens}{ones}{end}");
+            }
+    }
+}
