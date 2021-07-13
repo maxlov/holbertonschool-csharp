ea611033240e3ddefaca7ae4b7f7723c25e428db
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-13 15:01:32 -0400

    code_add task 12 fizzbuzz

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "";
+        for (var number = 0; number <= 100; number++)
+        {
+            text = "";
+            if (number % 3 == 0)
+                text += "Fizz";
+            if (number % 5 == 0)
+                text += "Buzz";
+            if (text.Length == 0)
+                text += number;
+            if (number != 100)
+                text += " ";
+            Console.Write(text);
+        }
+    }
+}
