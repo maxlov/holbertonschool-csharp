e3cfdd02e6a1185ccee13a0594c469a882062edf
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-13 12:10:19 -0400

    code_add task 4 print numbers 0 - 98 in hexadecimal

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (var number = 0; number < 99; number++)
+            Console.Write($"{number} = 0x{number:x}\n");
+    }
+}
