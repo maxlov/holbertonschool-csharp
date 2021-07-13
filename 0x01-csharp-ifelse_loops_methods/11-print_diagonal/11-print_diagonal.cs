3492df182983cb8952443f24a3775a45316bcf8e
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-13 14:32:25 -0400

    code_add task 11 printing a diag line

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+        {
+            Console.Write("\n");
+            return;
+        }
+        for (var spaces = 0; spaces < length; spaces++) {
+            for (var print_space = 0; print_space < spaces; print_space++)
+                Console.Write(" ");
+            Console.Write("\\\n");
+        }
+    }
+}
