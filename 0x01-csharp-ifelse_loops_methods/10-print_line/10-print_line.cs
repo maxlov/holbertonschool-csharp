884c6fdde216a9ebade234f456a7424ab3a95910
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-13 14:10:38 -0400

    code_add task 10, prints a line using recursion

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length <= 0)
+        {
+            Console.Write("\n");
+            return;
+        }
+        Console.Write("_");
+        PrintLine(length - 1);
+    }
+}
