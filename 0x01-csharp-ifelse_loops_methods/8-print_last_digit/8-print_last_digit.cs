c794d31cc4ff8c188719a183e188b6e5a19bf113
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-13 12:57:02 -0400

    code_add task 8 printing the last digit of a number

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        if (number < 0)
+            number *= -1;
+        while (number > 10)
+                number %= 10;
+        Console.Write(number);
+        return number;
+    }
+}
