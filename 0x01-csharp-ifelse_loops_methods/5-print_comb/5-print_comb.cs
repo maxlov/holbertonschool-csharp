ca8c5df7bec01c7f7664c3770fd1e5be15c4a19e
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-13 12:24:31 -0400

    code_add print comb, prints numbers 00 - 99 formatted

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var end = ", ";
+        for (var number = 0; number <= 99; number++)
+        {
+            if (number == 99)
+                end = "\n";
+            Console.Write($"{number:0#}{end}");
+        }
+    }
+}
