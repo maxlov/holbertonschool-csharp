301293225818bdc36343bd51d5e1aa5a081e569e
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-13 11:04:25 -0400

    code_add task 1, finds last digit of rng num

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int originalNumber = number;
+        if (number < 0)
+            while (number * -1 > 10)
+                number %= 10;
+        else
+            while (number > 10)
+                number %= 10;
+        if (number > 5)
+            Console.WriteLine($"The last digit of {originalNumber} is {number} and is greater than 5");
+        else if (number == 0)
+            Console.WriteLine($"The last digit of {originalNumber} is {number} and is 0");
+        else
+            Console.WriteLine($"The last digit of {originalNumber} is {number} and is less than 6 and not 0");
+    }
+}
