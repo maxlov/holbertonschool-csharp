9dd769d97776a7a7cedb3f7977c016dfda238e43
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-14 10:05:54 -0400

    code_add task 0, creates and prints an array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] newArray = new int[size];
+        for (int index = 0; index < size; index++)
+        {
+            newArray[index] = index;
+            if (index == size - 1)
+                Console.WriteLine(index);
+            else
+                Console.Write(index + " ");
+        }
+        if (size == 0)
+            Console.WriteLine();
+        return newArray;
+    }
+}
