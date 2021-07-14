50061d46aaf827889845330bb0b5190fdf37cba3
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-14 14:22:54 -0400

    code_add task 13 finds best score of dict

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string winner = "None";
+        int best = -1;
+
+        foreach (KeyValuePair<string, int> entry in myList)
+        {
+            if (entry.Value > best)
+            {
+                winner = entry.Key;
+                best = entry.Value;
+            }
+        }
+        return winner;
+    }
+}
