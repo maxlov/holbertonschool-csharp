dca9735d2904147a8f0bda6af9ccc94590bbadeb
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-14 15:05:44 -0400

    code_add task 14 make and print a 2d array

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] rectangle = new int[5, 5];
+
+        rectangle[2,2] = 1;
+
+        for (int col = 0; col < rectangle.GetLength(0); col++)
+            for (int row = 0; row < rectangle.GetLength(1); row++)
+            {
+                if (row == rectangle.GetLength(1) - 1)
+                    Console.Write(rectangle[col,row] + "\n");
+                else
+                    Console.Write(rectangle[col,row] + " ");
+            }
+    }
+}
