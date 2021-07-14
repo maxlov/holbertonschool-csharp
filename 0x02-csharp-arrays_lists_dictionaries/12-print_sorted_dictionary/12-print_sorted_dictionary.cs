71bdc4474c7f567d390f11b44701db79168c93f3
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-14 14:09:12 -0400

    code_add task 12, print dictionary values sorted

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> keyList = new List<string>(myDict.Keys);
+        keyList.Sort();
+        foreach (var key in keyList)
+            Console.WriteLine("{0}: {1}", key, myDict[key]);
+    }
+}
