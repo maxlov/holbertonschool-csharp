08f347ec5fecf5d67957bc0ca6ff0f0fba1ae1d2
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-14 15:16:14 -0400

    code_add task 15 squares a matrix

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        for (int col = 0; col < myMatrix.GetLength(0); col++)
+            for (int row = 0; row < myMatrix.GetLength(1); row++)
+                myMatrix[col,row] *= myMatrix[col,row];
+        return myMatrix;
+    }
+}
