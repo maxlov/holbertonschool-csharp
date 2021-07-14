bbb7574c44cbdf28ff70d6b648d5cfe6f8b09329
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-14 10:48:05 -0400

    code_add task 4, creates and prints list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        List<int> intList = new List<int>();
+        for (int index = 0; index < size; index++)
+            intList.Add(index);
+        Console.WriteLine(String.Join(" ", intList));
+        return intList;
+    }
+}
