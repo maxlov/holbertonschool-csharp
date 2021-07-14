6d1cabe20b0cce1db8a990f51128b81265e35955
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-14 10:29:20 -0400

    code_add task 3, printing array in reverse. I was bored so I used recursion

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        RecursiveReverse(array, 0);
+        Console.Write("\n");
+    }
+
+    static void RecursiveReverse(int[] array, int idx)
+    {
+        if (array == null || idx == array.Length)
+            return;
+        RecursiveReverse(array, idx + 1);
+        if (idx == 0)
+            Console.Write(array[idx]);
+        else
+            Console.Write(array[idx] + " ");
+        
+    }
+}
