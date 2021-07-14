d81b31bcec804ec8d85c3dc46f791a548aa07eec
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-14 12:50:07 -0400

    code_add task 8 count keys in dict

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        var count = 0;
+        foreach (var key in myDict)
+            count++;
+        return count;
+    }
+}
