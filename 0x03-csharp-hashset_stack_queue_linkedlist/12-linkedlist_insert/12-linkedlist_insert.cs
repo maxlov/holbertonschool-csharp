10f8df52b5e369d22c1bd4c582867fea57948d86
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-15 14:08:09 -0400

    code_add task 12, insert node in ordered list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        if (myLList.Count == 0)
+                return myLList.AddFirst(n);
+        LinkedListNode<int> current = myLList.First;
+        while (current.Next != null)
+        {
+            if (current.Value > n)
+                return myLList.AddBefore(current, n);
+            current = current.Next;
+        }
+        return current;
+    }
+}
