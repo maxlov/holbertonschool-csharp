49e4213d3e64b062da5e366dd5340db7c4b4d8cf
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-15 10:11:46 -0400

    code_add task one create new list of common elements

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> commonList = new List<int>();
+
+        foreach (int value in list1)
+            if (list2.Contains(value))
+                commonList.Add(value);
+        commonList.Sort();
+        return commonList;
+    }
+}
