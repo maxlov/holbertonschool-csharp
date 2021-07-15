78432061772acac0fd25915cd9d855857f8fc26a
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-15 11:21:37 -0400

    code_add create new linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    // Main - entry point
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> llist = new LinkedList<int>();
+        for (int i = 0; i < size; i++)
+        {
+            llist.AddLast(i);
+            Console.WriteLine(i);
+        }
+        return llist;
+    }
+}
