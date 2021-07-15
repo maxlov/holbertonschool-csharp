415ef04ebebede8dcc07693188180850599c027b
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-15 10:40:19 -0400

    code_add task 2 finds different elements of lists

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> diffList = new List<int>();
+
+        foreach (int value in list1)
+            if (!list2.Contains(value))
+                diffList.Add(value);
+        foreach (int value in list2)
+            if (!list1.Contains(value))
+                diffList.Add(value);
+        diffList.Sort();
+        return diffList;
+    }
+}
