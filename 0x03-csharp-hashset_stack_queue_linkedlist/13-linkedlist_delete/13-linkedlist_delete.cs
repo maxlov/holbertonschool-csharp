96aef4334c3e3feecc17cb76117133b75150bd73
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-15 15:02:51 -0400

    code_add 13 deletes node at index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> current = myLList.First;
+        for (int i = 0; current.Next != null; i++, current = current.Next)
+            if (i == index)
+            {
+                myLList.Remove(current);
+                return;
+            }
+    }
+}
