3e44e629152378b2736bcd75dab583d46c41166b
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-15 10:56:38 -0400

    code_add task 3, manages a stack

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        int count = aStack.Count;
+        Console.WriteLine($"Number of items: {count}");
+
+        if (count > 0)
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        else
+            Console.WriteLine("Stack is empty");
+
+        bool check = aStack.Contains(search);
+        Console.WriteLine($"Stack contains {search}: {check}");
+        if (check)
+            while (aStack.Pop() != search) {};
+
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
