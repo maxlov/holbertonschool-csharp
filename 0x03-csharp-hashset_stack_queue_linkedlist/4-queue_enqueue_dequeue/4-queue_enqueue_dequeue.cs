7f8aaecd148e11362b95a171060538df00891e71
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-15 11:03:01 -0400

    code_add task 4, manages a queue collection

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        int count = aQueue.Count;
+        Console.WriteLine($"Number of items: {count}");
+
+        if (count > 0)
+            Console.WriteLine($"Top item: {aQueue.Peek()}");
+        else
+            Console.WriteLine("Stack is empty");
+
+        bool check = aQueue.Contains(search);
+        Console.WriteLine($"Stack contains {search}: {check}");
+        if (check)
+            while (aQueue.Dequeue() != search) {};
+
+        aQueue.Enqueue(newItem);
+        return aQueue;
+    }
+}
