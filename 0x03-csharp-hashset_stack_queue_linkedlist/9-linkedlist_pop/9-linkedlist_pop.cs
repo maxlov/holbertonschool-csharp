f77947d57ec8404fb41a51d04f12f45a52378ece
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-15 13:02:08 -0400

    code_add task 11, pops list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int value = 0;
+        if (myLList != null)
+        {
+            value = myLList.First.Value;
+            myLList.RemoveFirst();
+        }
+        return value;
+    }
+}
