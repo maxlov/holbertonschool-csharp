88ebafc2e8a7844c0621cae18ed250bc78424000
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-15 10:01:08 -0400

    code_add task 0 finds sum of all unique values of list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    // Main - entry point
+    public static int Sum(List<int> myList)
+    {
+        List<int> usedList = new List<int>();
+        int total = 0;
+
+        foreach (int value in myList)
+            if (!usedList.Contains(value))
+            {
+                total += value;
+                usedList.Add(value);
+            }
+        return total;
+    }
+}
