2b40df0f8b99a5c93e5d4894e4052b2da4bb5f1c
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-15 13:10:46 -0400

    code_add task 10 get value at index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int i = 0;
+        foreach (int nodeVal in myLList)
+        {
+            if (i == n)
+                return nodeVal;
+            i++;
+        }
+        return 0;
+    }
+}
