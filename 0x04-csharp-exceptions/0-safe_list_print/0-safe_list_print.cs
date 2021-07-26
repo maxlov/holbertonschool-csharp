98f93a643b4e4f732abdfb74fea6a40e664bcf43
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-26 10:51:56 -0400

    code_add task 0 prints a list and handles exceptions

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int number = 0;
+        try
+        {
+            for (; number < n; number++)
+                Console.WriteLine(myList[number]);
+            return number;
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            return number;
+        }
+    }
+}
