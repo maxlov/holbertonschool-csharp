3c6661783ba4235c752572ba51a6c137a3389f70
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-26 11:01:04 -0400

    code_add task 1 divide numbers, catch 0 divisor

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int answer = 0;
+        try
+        {
+            answer = a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {answer}");
+        }
+    }
+}
