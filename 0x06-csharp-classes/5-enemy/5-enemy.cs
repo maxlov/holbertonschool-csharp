b0d7a2eb8f441c1b8fa676868436b3cd4b259378
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-27 13:52:10 -0400

    code_add 0x06 task 5 overide ToString for zombie class

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Enemies
+{
+    
+    ///<summary>Represents an enemy of type Zombie.</summary>
+    public class Zombie
+    {
+        ///<summary>Represents remaining health of a Zombie.</summary>
+        private int health;
+        private string name = "(No name)";
+
+        ///<summary>Private field name getter and setter.</summary>
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        ///<summary>Constructs a Zombie instance.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        ///<summary>Constructs a Zombie instance with a value.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+
+        ///<summary>Gets health of zombie instance.</summary>
+        public int GetHealth()
+        {
+            return this.health;
+        }
+
+        ///<summary>Overides default ToString.</summary>
+        public override string ToString()
+        {
+            return $"Zombie Name: {this.name} / Total Health: {this.health}";
+        }
+    }
+}
