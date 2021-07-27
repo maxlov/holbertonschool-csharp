800a05e47b4251a8972676497e3c9b468c8dfcaa
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-27 13:25:05 -0400

    code_add 0x06 task 2 added verification to zombie class

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Enemies
+{
+    
+    ///<summary>Represents an enemy of type Zombie.</summary>
+    public class Zombie
+    {
+        ///<summary>Represents remaining health of a Zombie.</summary>
+        public int health;
+
+        ///<summary>Constructs a Zombie instance</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        ///<summary>Constructs a Zombie instance with a value</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+    }
+}
