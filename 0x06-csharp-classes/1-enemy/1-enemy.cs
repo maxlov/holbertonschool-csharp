9d914c3188ad3547e506d230ebcc1f4ae4982cbd
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-07-27 12:28:39 -0400

    code_add 0x06 task 1, zombie now has public field health

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Enemies
+{
+    
+    ///<summary>Represents an enemy of type Zombie.</summary>
+    public class Zombie
+    {
+        ///<summary>Represents remaining health of a Zombie.</summary>
+        public int health;
+
+        ///<summary>Constructs a Zombie instance</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
