111e214ecd2b8a5a991e87e4f9da2c7ada409f54
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-08-02 10:05:03 -0400

    code_add 0x08 2-subclass checks if type is derived from type

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>Class that holds methods for objects.</summary>
+class Obj
+{
+    /// <summary>Returns true if type is derived from base type.</summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return derivedType.IsSubclassOf(baseType);
+    }
+}
