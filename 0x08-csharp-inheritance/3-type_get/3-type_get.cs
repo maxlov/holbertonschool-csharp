fb5c06a9ea86a833800a391b139cb013116ab64d
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-08-02 10:32:40 -0400

    code_add 0x08 task 3 get properties and methods of type

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Globalization;
+
+/// <summary>Class that holds methods for objects.</summary>
+class Obj
+{
+    /// <summary>Prints properties and methods of an object.</summary>
+    public static void Print(object myObj)
+    {
+        TypeInfo t = myObj.GetType().GetTypeInfo();
+        IEnumerable<PropertyInfo> pList = t.GetProperties();
+        IEnumerable<MethodInfo> mList = t.GetMethods();
+
+        StringBuilder sb = new StringBuilder();
+        
+        sb.Append($"{t.Name} Properties:");
+        foreach (PropertyInfo p in pList)
+        {
+
+            sb.Append("\n" + p.Name);
+        }
+        sb.Append($"\n{t.Name} Methods:");
+        foreach (MethodInfo m in mList)
+        {
+            sb.Append("\n" + m.Name);
+        }
+        
+        Console.WriteLine(sb.ToString());
+    }
+}
