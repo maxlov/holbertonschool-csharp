4a80c34acae3b140a09ed0a51f7fa3bf331fa76f
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-08-02 09:55:07 -0400

    code_add 0x08 task 1 check if array is in inheritance tree of array

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>Class that holds methods for objects.</summary>
+class Obj
+{
+    /// <summary>Returns true if object is in inheritance tree of array.</summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is Array;
+    }
+}
