7664a71234c4c75bf476c5fe5d1bffe8070cbd54
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-08-02 10:43:42 -0400

    code_add 0x08 task 5 basic shape class

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+/// <summary>Basic shape class.</summary>
+class Shape
+{
+    /// <summary>Finds are of shape.</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
