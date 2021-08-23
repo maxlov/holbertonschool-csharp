5e79f3b93b51276423cda044403f2496a1fe47a8
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-08-23 15:29:20 -0400

    code_fix 0x09 task 20 missing null check

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -6,7 +6,7 @@ class MatrixMath
     /// <summary>Calculates rotation of matrix.</summary>
     public static double[,] Rotate2D(double[,] matrix, double angle)
     {
-        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        if (matrix == null || matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
             return new double[,] { { -1 } };
 
         double[,] result = new double[2, 2];
