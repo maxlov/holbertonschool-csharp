2fb4e013f0b4e599d14212ad3e7b2b4cf772542a
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-08-24 11:55:43 -0400

    code_fix 0x09 task 20 reduntant result 0 set

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -17,7 +17,7 @@ class MatrixMath
         int i, j, k;
 
         for (i = 0; i < 2; i++)
-            for (j = 0, result[i, j] = 0; j < 2; j++)
+            for (j = 0; j < 2; j++)
                 for (k = 0; k < 2; k++)
                     result[i, j] += Math.Round(matrix[i, k] * rotation[k, j], 2);
         return result;
