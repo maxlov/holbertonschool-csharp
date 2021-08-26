517c0bf02a23cc78257b415d102923590b999cec
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-08-26 12:23:43 -0400

    code_fix removed rounding at end

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -15,7 +15,7 @@ class MatrixMath
             double a = Math.Round(matrix[0,0] * (matrix[1,1] * matrix[2,2] - matrix[1,2] * matrix[2,1]), 2);
             double b = Math.Round(matrix[0,1] * (matrix[1,0] * matrix[2,2] - matrix[1,2] * matrix[2,0]), 2);
             double c = Math.Round(matrix[0,2] * (matrix[1,0] * matrix[2,1] - matrix[2,2] * matrix[2,0]), 2);
-            return Math.Round(a - b + c, 2);
+            return a - b + c;
         }
         return -1;
     }
