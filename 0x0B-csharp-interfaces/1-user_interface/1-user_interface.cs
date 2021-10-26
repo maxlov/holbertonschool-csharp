e81f2386115a6427205d32b6c6d10ab201ca431e
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-10-26 12:44:54 -0400

    [Create] Task 1

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{name} is a {GetType()}";
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+    public void Interact() 
+    {
+        throw new NotImplementedException();
+    }
+    public void Break()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public void Collect()
+    {
+        throw new NotImplementedException();
+    }
+}
