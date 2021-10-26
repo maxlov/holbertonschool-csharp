b222f6f2f6fa76449117a9e29daf4b9f2906f479
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-10-26 12:58:20 -0400

    [Create] task 2, door class

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{name} is a {GetType()}";
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+class Door : Base, IInteractive
+{
+    public Door(string doorName = "Door")
+    {
+        name = doorName;
+    }
+
+    public void Interact() 
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
